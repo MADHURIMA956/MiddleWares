051ee824478b2c68dcfbf09e458556c2d51aa1b9
Madhurima956 <madhurimabanerjee97@gmail.com>
2021-11-19 23:42:56 +0530

    update

--- app.js
+++ app.js
@@ -28,6 +28,24 @@ app.get('/:id',(req,res) => {
     res.send(newBook)
 })
 
+// patch
+
+app.patch('/:id' , (req,res) => {
+
+    const newBook = books.map((book) => {
+         
+        if(req.params.id === book.id){
+            if( req?.body?.id )book.id = req.body.id;
+            if( req?.body?.book_name )book.book_name = req.body.book_name;
+            if( req?.body?.page_no )book.page_no = req.body.page_no;
+            if( req?.body?.published_year )book.published_year = req.body.published_year;
+        }
+        return book;
+    });
+      res.send(newBook);
+})
+
+
 app.listen(2345, function(){
     console.log('Listenning on port 2345')
 })
